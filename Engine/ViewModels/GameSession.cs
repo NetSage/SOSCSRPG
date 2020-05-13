eaac09a1982cd95a055a91057b728ec2ef84b443
Alexander Smith <alexsmith@purelymail.com>
2020-05-13 11:09:33 -0500

    Created ViewModel and player constructor (lesson 3.4)

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+
+            CurrentPlayer.Name = "Alex";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
