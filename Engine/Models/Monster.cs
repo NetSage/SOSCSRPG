dd04208520d4c3098ab8d6168cbb20a31c1aa2d7
Alexander Smith <alexsmith@purelymail.com>
2020-05-25 17:02:38 -0500

    Created Monsters (lessons 7.1)

---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+        private int _hitPoints;
+
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int HitPoints 
+        {
+            get
+            {
+                return _hitPoints;
+            }
+            private set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+        public Monster(string name, string imageName, int maximumHitPoint,
+            int hitPoints, int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
+            MaximumHitPoints = maximumHitPoint;
+            HitPoints = hitPoints;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+            Inventory = new ObservableCollection<ItemQuantity>();
+        }
+
+    }
+}
---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        //none deterministic version of RNG
+        private static readonly RNGCryptoServiceProvider _generator = new RandomNumberGenerator();
+
+        public static int NumberBetween(int minimuValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueofRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            //Math to ensure it's always between 0.0 and 0.9999999 as "1" causes problems
+            double multiplier = Math.Max(0, (asciiValueofRandomCharacter / 255d) - 0.00000000001d);
+
+            //We need to add one to the range to allow for the round done with math.floor
+            int range = maximumValue - minimuValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimuValue + randomValueInRange);
+        }
+
+        //Simple random generator that is more easily predicted
+        //not used just here if I want to use it
+
+        private static readonly Random _simpleGenerator = new Random();
+
+        public static int SimpleNumberBetweeen(int minimumValue, int maximumValue)
+        {
+            return _simpleGenerator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
